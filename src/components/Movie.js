d63e467d036888aeb02252f37284c7a2a49ca1dd
Daniel Dytert <danieldytert@gmail.com>
2024-03-09 23:24:46 +0100

    show genres and image

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -16,8 +16,17 @@ const Movie = () => {
       .then((response) => response.json())
       .then((data) => {
         setMovie(data);
+      })
+      .catch(err => {
+        console.log(err);
       });
   }, [id]);
+
+  if (movie.genres) {
+    movie.genres = Object.values(movie.genres);
+  } else {
+    movie.genres = [];
+  }
 
   return (
     <div>
@@ -31,7 +40,18 @@ const Movie = () => {
           Rated: {movie.mpaa_rating}
         </em>
       </small>
+      <br />
+      {movie.genres.map((g) => (
+        <span key={g.genre} className="badge bg-secondary me-2">{g.genre}</span>
+      ))}
       <hr />
+
+      {movie.image !== "" &&
+        <div className="mb-3">
+          <img src={`https://image.tmdb.org/t/p/w200/${movie.image}`} alt="poster" />
+        </div>
+      }
+
       {movie.description}
     </div>
   );
